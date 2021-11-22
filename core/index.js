586279126c7c4cc157a5f46941e085113cf3b7ad
Saurabh Pathak <4saurabhpathak@gmail.com>
2021-11-22 10:16:13 +0530

    implemented multiple reducers

--- core/index.js
+++ core/index.js
@@ -1,38 +1,93 @@
 const redux = require("redux");
 
 const createStore = redux.createStore;
+const combineReducers = redux.combineReducers; // to combine multiple reducer and make one to create store
+
 console.log("hii");
 
 // action
 const BUY_CAKE = "BUY_CAKE";
+const BUY_ICECREAM = "BUY_ICECREAM";
 
 //action creator
-function buy() {
+function buyCake() {
   // action
   return {
     // properties which defines what to do with states of store
     type: BUY_CAKE,
+
     info: "first redux",
   };
 }
 
-const initialState = {
+function buyIcecream() {
+  return { type: BUY_ICECREAM };
+}
+
+// const initialState = {
+//   numOfCakes: 20,
+//   numOfIceCream: 50,
+// };
+
+const initialCakeState = {
   numOfCakes: 20,
 };
 
-const reducer = (state = initialState, action) => {
+const initialIceCreamState = {
+  numOfIceCream: 50,
+};
+
+// const reducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case BUY_CAKE:
+//       return {
+//         ...state,
+//         numOfCakes: state.numOfCakes - 1, // retruning new object not state object
+//       };
+//     case BUY_ICECREAM:
+//       return {
+//         ...state,
+//         numOfIceCream: state.numOfIceCream - 1,
+//       };
+//     default:
+//       return state;
+//   }
+// };
+
+const cakeReducer = (state = initialCakeState, action) => {
   switch (action.type) {
     case BUY_CAKE:
       return {
         ...state,
         numOfCakes: state.numOfCakes - 1, // retruning new object not state object
       };
+
     default:
       return state;
   }
 };
 
-const store = createStore(reducer);
+const IcecreamReducer = (state = initialIceCreamState, action) => {
+  switch (action.type) {
+    case BUY_ICECREAM:
+      return {
+        ...state,
+        numOfIceCream: state.numOfIceCream - 1, // retruning new object not state object
+      };
+
+    default:
+      return state;
+  }
+};
+
+// create root reducer which call a method combineReducer
+//to sum up all the reducer which take a object as parameter
+const rootReducer = combineReducers({
+  cake: cakeReducer,
+  iceCream: IcecreamReducer,
+});
+
+const store = createStore(rootReducer);
 
 console.log("initial state", store.getState());
 
@@ -40,8 +95,9 @@ const unsubscribe = store.subscribe(() => {
   console.log("upadated state", store.getState());
 });
 
-store.dispatch(buy());
-store.dispatch(buy());
-store.dispatch(buy());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyIcecream());
+store.dispatch(buyIcecream());
 
 unsubscribe();
