08938efde7ffdf3e04050fee2567dca94a1eba69
Saurabh Pathak <4saurabhpathak@gmail.com>
2021-11-22 13:54:57 +0530

    react-redux

---
+++ react-reduxdemo/src/components/CakeContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function CakeContainer() {
+  return (
+    <div>
+      <h2>Cake shop</h2>
+      <button> Buy cake</button>
+    </div>
+  );
+}
+
+export default CakeContainer;
---
+++ react-reduxdemo/src/redux/cake/cakeReducer.js
@@ -0,0 +1,18 @@
+ import { BUY_CAKE } from "./cakeType"
+ const initalState = {
+     numOfCakes : 10
+ }
+
+
+ const cakeReducer = (state = initalState, action)=>{
+     switch(action.type) {
+         case BUY_CAKE:
+             return{
+                 numOfCakes= state.numOfCakes -1
+             }
+             default: 
+             return state
+     }
+ }
+
+ export default cakeReducer
